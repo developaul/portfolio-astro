b0ee60dd15957aaac307b288100608bb21172130
Paul Chávez <paulchavezromero20@gmail.com>
2023-11-13 17:42:20 -0500

    chore: Header component added

---
+++ src/constants/links.ts
@@ -0,0 +1,16 @@
+import { ILink } from "src/interfaces"
+
+export const navBarLinks: ILink.NavbarLink[] = [
+  {
+    to: '#projects',
+    title: 'Proyectos'
+  },
+  {
+    to: '#about',
+    title: 'Acerca de mi'
+  },
+  {
+    to: '#contact',
+    title: 'Contactame'
+  }
+]
---
+++ src/interfaces/link.ts
@@ -0,0 +1,4 @@
+export interface NavbarLink {
+  to: string;
+  title: string
+}

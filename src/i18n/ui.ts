dfc4b5da255f616ee20f756f099c4835c9d2d4e6
Paul Chavez <paulchavezromero20@gmail.com>
2024-05-02 00:04:16 -0500

    chore: i18n configured

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+import en from '@i18n/locales/en.json';
+import es from '@i18n/locales/es.json';
+
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en,
+  es,
+} as const;

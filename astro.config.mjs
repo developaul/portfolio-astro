21aadd827edd2c3d16ddbc07b45ea4b7a0fdd420
Paul Chavez <paulchavezromero20@gmail.com>
2024-05-19 14:41:33 -0500

    chore: @astrojs/vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
-    locales: ["es", "en"],
-  }
+    locales: ["es", "en"]
+  },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
